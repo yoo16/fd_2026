6690d3b9efe95d609cd8c05486d53db23d0a9d45
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-07 16:54:29 +0900

    update

--- react-hello/src/components/Header.jsx
+++ react-hello/src/components/Header.jsx
@@ -2,10 +2,14 @@
 // rafce と入力して、Enter を押すと、以下のコードが出力される
 
 import styles from './Header.module.css'
+import logo from '../assets/logo.svg'
 
 function Header() {
     return (
         <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src={logo} alt="Logo" />
+            </div>
             <div className={styles.title}>Anime Station</div>
         </header>
     )
