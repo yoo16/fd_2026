80a104df5009f4d448f7d4bb7a6912664c830657
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-07 15:00:56 +0900

    update

--- react-hello/src/App.jsx
+++ react-hello/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-      <div>こんにちは</div>
-    </>
+    <div>
+      <main>
+        <h2>Anime Station</h2>
+        <div>ここになんか作品</div>
+      </main>
+    </div>
   )
 }
 
