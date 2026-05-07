5be3cc903c8c5d48504515cd29091a39b3fe8f9e
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-07 15:56:33 +0900

    update

---
+++ react-hello/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// ES7 React/Redux/GraphQL/React-Native snippets をインストール
+// rafce と入力して、Enter を押すと、以下のコードが出力される
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
