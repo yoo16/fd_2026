52f84e7806f1f9b5ffa57cc64a1067883cbbd5c2
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-14 16:55:20 +0900

    update

--- react-hello/src/components/WorkCard.jsx
+++ react-hello/src/components/WorkCard.jsx
@@ -3,6 +3,10 @@ import styles from './WorkCard.module.css'
 function WorkCard({ work, onSelect }) {
     return (
         <div className={styles.card} onClick={() => onSelect(work)}>
+            {/* 画像領域 */}
+            <div>
+                <img src={work.image} alt={work.title} />
+            </div>
             {work.title}
         </div>
     )
