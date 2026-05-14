ad90e26ca009dea1a2c9a245404a73bbbeb3ab5d
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-14 15:26:10 +0900

    update

--- react-hello/src/App.jsx
+++ react-hello/src/App.jsx
@@ -2,6 +2,8 @@ import styles from './App.module.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import WorkList from './components/WorkList';
+// モックデータの読み込み
+import { works } from './data/works';
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
 
       <main className={styles.main}>
         <h2 className={styles.sectionTitle}>Anime Station</h2>
-        <WorkList />
+        <WorkList works={works} />
       </main>
 
       <Footer />
