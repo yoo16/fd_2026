156e6e9f5d63139e33b2ae97058482bc5fecdb64
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-21 16:12:17 +0900

    update

---
+++ react-hello/src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+    return (
+        <div>
+            <section>
+                検索キーワード
+            </section>
+            <section>
+                ジャンル選択
+            </section>
+        </div>
+    )
+}
+
+export default SearchBar
