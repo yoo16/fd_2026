b4c18e7da80b7393dcc13e35bb6e6b095060c2ec
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-28 16:35:39 +0900

    update

--- react-hello/src/hooks/useWorks.js
+++ react-hello/src/hooks/useWorks.js
@@ -4,9 +4,15 @@ function useWorks() {
     // 作品データを管理するための状態
     const [works, setWorks] = useState([])
 
-    // コンポーネントが初回読み込まれたときにAPIサーバから作品データを取得
+    // コンポーネントが初回読み込まれたときに
     useEffect(() => {
-
+        // APIサーバから作品データを取得
+        async function fetchWorks() {
+            const response = await fetch('http://localhost:3001/works')
+            const data = await response.json()
+            setWorks(data)
+        }
+        fetchWorks()
     }, [])
 
     // 作品データを返す
