c196930e0fa02b8e37547954e344497e01be5b5e
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-04 15:15:51 +0900

    update

--- react-hello/src/components/LoadingModal.jsx
+++ react-hello/src/components/LoadingModal.jsx
@@ -4,8 +4,12 @@ import styles from './LoadingModal.module.css'
 function LoadingModal() {
     return (
         <div className={styles.overlay}>
-            <Spinner />
-            <p>読み込み中...</p>
+            <div className={styles.modal}>
+                <Spinner />
+                <p className={styles.message}>
+                    読み込み中...
+                </p>
+            </div>
         </div>
     )
 }
