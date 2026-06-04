0cdcbc491a7af7b341d9172a35a67c37d6c7a46e
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-04 16:20:35 +0900

    update

--- react-hello/src/components/FlashMessage.jsx
+++ react-hello/src/components/FlashMessage.jsx
@@ -1,6 +1,16 @@
 import styles from './FlashMessage.module.css'
+import { useState, useEffect } from 'react'
 
 function FlashMessage({ message, type = 'error' }) {
+    const [dissmissed, setDismissed] = useState(false)
+    const duration = 3000;
+
+    const timer = setTimeout(() => {
+        setDismissed(true)
+    }, duration)
+
+    if (!message || dissmissed) return null
+
     return (
         <div className={`${styles.flash} ${styles[type]}`}>
             <div>{message}</div>
