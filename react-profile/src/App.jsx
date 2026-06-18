440a39ab8306d5139c72b679bc904cc2a490c85a
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-18 15:16:08 +0900

    update

--- react-profile/src/App.jsx
+++ react-profile/src/App.jsx
@@ -1,10 +1,11 @@
 // Layout コンポーネントをインポート
 import Layout from './components/Layout'
+import Home from './pages/Home'
 
 function App() {
   return (
     <Layout>
-      メインページ
+      <Home />
     </Layout>
   )
 }
