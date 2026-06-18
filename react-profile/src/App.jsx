da40c5649221ff6e50fae3eb7ea2c5dda5228fba
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-18 15:26:39 +0900

    update

--- react-profile/src/App.jsx
+++ react-profile/src/App.jsx
@@ -1,11 +1,16 @@
+// useState をインポート
+import { useState } from 'react'
 // Layout コンポーネントをインポート
 import Layout from './components/Layout'
 import Home from './pages/Home'
 
 function App() {
+  // useState を使ってページの状態を管理: page
+  const [page, setPage] = useState('home')
+
   return (
     <Layout>
-      <Home />
+      {page === 'home' && <Home /> }
     </Layout>
   )
 }
